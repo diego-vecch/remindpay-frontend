1ce1abd0e19ff06a95d7b1302cdd716c9a912535
Diego <josediegoferia@gmail.com>
2023-08-31 12:30:55 -0300

    ci: config files and install dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,29 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        'purple-light': '#E5D1FF',
+        'purple-half-light': '#C293FF',
+        'purple-regular': '#9B56F3',
+        'purple-half-dark': '#7F39D9',
+        'purple-dark': '#361165',
+        'purple-fulldark': '#220629',
+        'blue-regular': '#440298'
+      },
+      screens: {
+        'vy-sm': '360px'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
