a8c15d4d3acab7b35ad47cf2a612522e45eed252
marcelo <marcelocamusso@hotmail.com>
2024-01-13 15:45:49 -0300

    fix: create form contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,12 @@
+import ContactPage from '@/components/Form'
 import { NavbarHomepage } from '@/components/NavbarHomepage'
+
 export default function page (): JSX.Element {
   return (
     <main className='h-full flex flex-col justify-center w-full'>
       <NavbarHomepage />
-      <div>Contact</div>
+      
+      <ContactPage />
     </main>
   )
 }
