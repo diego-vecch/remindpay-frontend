78144cef3fa047dff259147e7ba529d24d952bdf
Diego <josediegoferia@gmail.com>
2024-01-15 10:42:47 -0300

    fix: update style and components of login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-
+'use client'
+import { useState } from 'react'
+import { ButtonForm } from './ButtonForm'
+import { EyeOpenPasswordForm, EyeClosePasswordForm } from './Icons/EyePasswordForm'
 const Login: React.FC = () => {
+  const [view, setview] = useState(false)
+  const password = view ? 'text' : 'password'
   return (
     <div className='h-full flex flex-col pt-20 justify-center items-center w-full'>
-      <div className='mb-8 flex flex-col justify-center'>
+      <div className='mb-8 flex flex-col gap-2 justify-center w-96'>
         <div className='py-6'>
-          <h4 className='text-center text-2xl text-violet-950'>Login</h4>
+          <h4 className='text-center text-3xl text-violet-900'>Login</h4>
         </div>
-        <form action='' className='w-full max-w-md bg-purple-0 rounded-md  '>
+        <form action='' className='w-full max-w-md rounded-md flex flex-col gap-4 '>
           <div className='mb-4'>
             <input
               type='text' id='emailorusername'
@@ -16,15 +20,30 @@ const Login: React.FC = () => {
               className='w-full p-2 border rounded-md bg-purple-500 bg-opacity-5 placeholder-purple-700 border-purple-900  focus:outline-none focus:border-violet-600 focus:placeholder-violet-500 focus:bg-opacity-0 text-purple-800' placeholder='Email or Username:'
             />
           </div>
-          <div className='mb-4'>
+          <div className='mb-4 flex'>
             <input
-              type='password' id='password' required
+              type={password}
+              required
               autoComplete='off'
               className='w-full p-2 border rounded-md bg-purple-500 bg-opacity-5 placeholder-purple-700 border-purple-900 focus:outline-none focus:border-violet-600 focus:placeholder-violet-500 focus:bg-opacity-0 text-purple-800' placeholder='Password:'
             />
+            <div className='pt-1 z-10'>
+              <button
+                className='absolute ml-3' onClick={(e) => {
+                  e.preventDefault()
+                  setview(!view)
+                }}
+              >
+                {view && (<EyeOpenPasswordForm />)}
+                {!view && (<EyeClosePasswordForm />)}
+
+              </button>
+            </div>
+
           </div>
-          <button type='submit' className='bg-purple-700 text-white p-2 w-full rounded-md mb-2'>Log in</button>
-          <button type='submit' className='bg-transparent text-purple-700 p-2 w-full rounded-md'>Forgot your password?</button>
+          <ButtonForm nameButton='Log in' />
+          <div className='w-full flex justify-center'><button type='submit' className='bg-transparent text-purple-700 rounded-md hover:underline'>Forgot your password?</button></div>
+
         </form>
       </div>
     </div>
