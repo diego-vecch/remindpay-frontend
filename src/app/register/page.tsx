d0009b5682bae4307d11ef5721c4fc6e6a681ac9
marcelo <marcelocamusso@hotmail.com>
2024-01-18 09:11:27 -0300

    feat: Create Register.

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import Register from '@/components/Register'
+
+export default function page (): JSX.Element {
+  return (
+    <main className='h-screen flex flex-col justify-center w-full'>
+      <Register />
+    </main>
+  )
+}
