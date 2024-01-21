4d2455ba416a599777b14cb9ed9bca4be36d36a9
Diego <josediegoferia@gmail.com>
2024-01-21 19:15:56 -0300

    bugfix: login | first validate, error of promises, add jwt and redirect

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,44 +5,55 @@ import { EyeOpenPasswordForm, EyeClosePasswordForm } from './Icons/EyePasswordFo
 
 const Login: React.FC = () => {
   const [view, setview] = useState(false)
-  const password = view ? 'text' : 'password'
+  const typeInput = view ? 'text' : 'password'
   const [email, setEmail] = useState('')
-  const [passwordUser, setPasswordUser] = useState('')
-  const [apiResponse, setApiResponse] = useState(null)
+  const [password, setPassword] = useState('')
+  const [responseLogin, setResponseLogin] = useState({
+    status: '',
+    message: ''
+  })
 
   const handleSubmit = (): void => {
-    try {
-      const response = fetch('http://localhost:8000/api/v1/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email,
-          password: passwordUser
-        })
-      }).then((res) => {
-        const data = res.json()
-        setApiResponse(data)
-        console.log(apiResponse)
+    const URL = process.env.NEXT_PUBLIC_URL_LOGIN ?? 'localhost:8000/api/v1/auth/login'
+    void fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
       })
-      if (response.ok) {
-        // Manejar la respuesta de éxito, por ejemplo, redirigir al usuario.
-        console.log('Inicio de sesión exitoso')
-        // Puedes redirigir al usuario o realizar otras acciones aquí.
-      } else {
-        // Manejar errores de autenticación.
-        console.error('Error al iniciar sesión:', response.statusText)
+    }).then(async res => {
+      const data = await res.json()
+      if (!res.ok) {
+        setResponseLogin({
+          status: data.status,
+          message: data.message
+        })
       }
-    } catch (error) {
-      console.error('Error al enviar la solicitud:', error)
-    }
+      return data
+    }).then(response => {
+      sessionStorage.setItem('JWT', response.token)
+      setResponseLogin({
+        status: response.status,
+        message: response.message
+      })
+      redirect()
+    })
   }
+
+  const redirect = (): void => global.location.replace('http://localhost:3000/')
+
   return (
     <div className='h-full flex flex-col pt-20 justify-center items-center w-full'>
       <div className='mb-8 flex flex-col gap-2 justify-center w-96'>
-        <div className='py-6'>
-          <h4 className='text-center text-3xl text-violet-900'>Login</h4>
+        <div className='pt-4 pb-1'>
+          <h4 className='text-center text-3xl text-violet-900 mb-2'>Login</h4>
+          {responseLogin.status === '' && (<article className='h-10 flex justify-center' />)}
+          {responseLogin.status === 'error' && (<article className='text-red-600 bg-red-400 bg-opacity-20 text-center h-10 flex items-center justify-center'>{responseLogin.message}</article>)}
+          {responseLogin.status === 'success' && (<article className='text-green-600 bg-green-400 bg-opacity-20 text-center h-10 flex items-center justify-center'>{responseLogin.message}</article>)}
+          {(responseLogin.status !== 'success' && responseLogin.status !== '' && responseLogin.status !== 'error') && (<article className='text-red-600 bg-red-400 bg-opacity-20 text-center h-10 flex items-center justify-center'>{responseLogin.message}</article>)}
         </div>
         <form action='' className='w-full max-w-md rounded-md flex flex-col gap-4 '>
           <div className='mb-4'>
@@ -57,16 +68,17 @@ const Login: React.FC = () => {
           </div>
           <div className='mb-4 flex'>
             <input
-              type='password'
+              type={typeInput}
               required
               autoComplete='off'
-              value={passwordUser}
-              onChange={(e) => setPasswordUser(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className='w-full p-2 border rounded-md bg-purple-500 bg-opacity-5 placeholder-purple-700 border-purple-900 focus:outline-none focus:border-violet-600 focus:placeholder-violet-500 focus:bg-opacity-0 text-purple-800' placeholder='Password:'
             />
             <div className='pt-1 z-10'>
               <button
-                className='absolute ml-3' onClick={(e) => {
+                className='absolute ml-3'
+                onClick={(e) => {
                   e.preventDefault()
                   setview(!view)
                 }}
@@ -81,7 +93,7 @@ const Login: React.FC = () => {
           <ButtonForm
             click={(e: { preventDefault: () => void }) => {
               e.preventDefault()
-              handleSubmit()
+              void handleSubmit()
             }} nameButton='Log in'
           />
           <div className='w-full flex justify-center'>
